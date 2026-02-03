22ad38ed901e709a23b64f72fd4ac8ed5b89afc2
BonjourPointDoc <valentinesoddu86@gmail.com>
2026-02-03 16:41:21 +0100

    Fixed CORS issue

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,11 @@ import { ClassController } from './adapters/driving/classController';
 
 dotenv.config();
 
+const cors = require('cors');
 const app = express();
+app.use(express.json());
+app.use(cors({ origin: 'http://localhost:9000' }));
+
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
